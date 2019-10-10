64eb811eb96ed5ae08bd035e8c44df7e8cf89c7f
daramedeiros <daraamedeiros@outlook.com>
2019-10-10 14:33:45 -0300

    exercicios de array

--- woliveiras-fullstack/app.js
+++ woliveiras-fullstack/app.js
@@ -8,6 +8,7 @@ let altura = 1.60
 
 let imc = peso / (altura * altura)
 
+
 //constantes - valores constantes que não mudam - ex:ContaBancária
 let saldoBancario = 1000
 const taxa = 1
@@ -21,6 +22,7 @@ console.log(`Segundo saque ${saldoBancario}`)
 saldoBancario = (saldoBancario - 100) - taxa
 console.log(`Terceiro saque ${saldoBancario}`)
 
+
 //tipos de dados
 const idade = 23
 
@@ -28,12 +30,14 @@ let dinheiroNaConta = 700.00
 
 const nome = "Dara Medeiros"
 
+
 //strings 
 console.log(typeof nome)
 
 console.log(typeof dinheiroNaConta)
 
 console.log(typeof idade)
+
 
 //booleans
 let idadeAtual = 23
@@ -46,8 +50,8 @@ let menorDeIdade = 15
 
 console.log(menorDeIdade >= maiorIdade)
 
-//Null e undefined
 
+//Null e undefined
 let variavelSemNada = null
 
 let outraVariavel = variavelSemNada
@@ -58,3 +62,67 @@ console.log(variavelSemNada)
 let naoNasciAinda = undefined
 
 console.log(typeof naoNasciAinda)
+
+
+//Objects
+let pessoa = {
+    nome: "Dara",
+    idade: 23,
+    profissao:"Fullstack Dev",
+}
+
+//acessando os dados dentro do um objeto
+//objeto + ponto + nome da chave.
+console.log(pessoa.nome)
+console.log(pessoa.idade)
+console.log(pessoa.profissao)
+
+console.log(pessoa)
+
+
+//Coleções 
+let pessoa1 = {
+    nome: "Rafael",
+    idade: 24,
+    profissao:"Back-end Dev",
+}
+
+let pessoa3 = {
+    nome: "Dedeu",
+    idade: 17,
+    profissao:"Tecnico de Redes",
+}
+
+
+//Arrays
+let lista = [] //dentro de [] é onde vão os itens da lista, separado por vírgula
+
+let listaDeCompras = ["Alface", "Tomate", "Batata doce", "Pêra", "Maçã"]
+
+
+//Vamos pensar no caso anterior, onde nós precisamos de várias pessoas em nosso programa. Pessoas são objetos que possuem dados chaveados e precisamos criar uma lista com isso dentro agora.
+
+let pessoas = [
+    {
+        nome: "Dara",
+        idade: 23,
+        profissao:"Fullstack Dev",
+    }, 
+    {
+        nome: "Rafael",
+        idade: 24,
+        profissao:"Back-end Dev",
+    },
+    {
+        nome: "Dedeu",
+        idade: 17,
+        profissao:"Tecnico de Redes",
+    }
+    
+]
+
+//Agora temos uma lista de pessoas e cada item dessa lista é um objeto com dados. 
+
+//Para acessar os itens dessa lista, fazemos lista[posição]
+
+console.log(pessoas[0])
